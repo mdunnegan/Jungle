1a0988055be46035b4b1d46fe651f5d25841deef
Michael Dunnegan <mdunnegan@spicy.io>
2014-07-06 10:42:43 -0400

    testing git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jungle::Application.config.secret_token = '82dc539e0139cebb1bd52c45df313cc52d334780c0ee8846bdd16ff3dd7cc5eb922a5dc7745ede44c494fad0c4b8955267d513b96a8dfdcb7714d87e8dbb8520'
